ee24b1b4d0dc1131023bef2c8e7875f8a8ae2cf3
unknown <pelumisunday@gmail.com>
2020-11-29 16:27:06 +0100

    feat: include client details

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,14 @@
-// import axios from "axios";
-// import config from "../config";
+import axios from "axios";
+import config from "../config";
 
-// export default {
-//   pingServerOne: () => {
-//     let method = "post";
-//     let url = config.serverUriOne + "/ping";
-//     return axios({
-//       method,
-//       url,
-//       headers: { "ONB-CID": config.clientId }
-//     });
-//   },
-//   pingServerTwo: () => {
-//     let method = "post";
-//     let url = config.serverUriTwo + "/ping";
-//     return axios({
-//       method,
-//       url,
-//       headers: { "ONB-CID": config.clientId }
-//     });
-//   },
-// };
+export default {
+  pingUnsplash: () => {
+    let method = "post";
+    let url = config.serverURI + "/ping";
+    return axios({
+      method,
+      url,
+      headers: { Authorization: "Client-ID " + config.clientKey }
+    });
+  }
+};
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 export default {
   serverURI: process.env.VUE_APP_URL,
-  clientSecret: process.env.VUE_APP_CLIENT_SECRET
+  clientSecret: process.env.VUE_APP_CLIENT_SECRET,
+  clientKey: process.env.VUE_APP_CLIENT_KEY
 };
